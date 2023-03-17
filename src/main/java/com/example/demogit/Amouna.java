b994395f8d6266619aa3812810720197951db31f
salahmedamin2019 <salahmedamin2019@gmail.com>
2023-03-17 21:17:29 +0100

    aamalt projet b esmi

---
+++ src/main/java/com/example/demogit/Amouna.java
@@ -0,0 +1,9 @@
+package com.example.demogit;
+
+public class Amouna {
+    String nom;
+    String prenom;
+    public Amouna(String nom, String prenom){
+        System.out.println("ESMI "+nom+" W LA9ABI "+prenom);
+    }
+}
